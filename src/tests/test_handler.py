5291934568434a612cfdcf5a633d0c57be45ddab
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-11-17 13:43:03 -0800

    remove policy and use default

--- src/tests/test_handler.py
+++ src/tests/test_handler.py
@@ -329,44 +329,6 @@ class TestHandler(TestCase):
             {"action": "make_kms_key", "key_project": "WQP-EXTERNAL", "key_stage": "TEST"},
             self.context)
         mock_client.create_key.assert_called_once_with(
-            Policy={
-                'Version': '2012-10-17',
-                'Id': 'key-consolepolicy-3',
-                'Statement': [
-                    {
-                        'Sid': 'Enable IAM User Permissions', 'Effect': 'Allow',
-                        'Principal': {'AWS': 'arn:aws:iam::None:root'},
-                        'Action': 'kms:*', 'Resource': '*'
-                    },
-                    {
-                        'Sid': 'Allow access for Key Administrators', 'Effect': 'Allow',
-                        'Principal': {'AWS': 'arn:aws:iam::None:role/Ec2-Role'},
-                        'Action': ['kms:Create*', 'kms:Describe*', 'kms:Enable*', 'kms:List*', 'kms:Put*',
-                                   'kms:Update*', 'kms:Revoke*', 'kms:Disable*', 'kms:Get*', 'kms:Delete*',
-                                   'kms:TagResource', 'kms:UntagResource', 'kms:ScheduleKeyDeletion',
-                                   'kms:CancelKeyDeletion'], 'Resource': '*'
-                    },
-                    {
-                        'Sid': 'Allow use of the key', 'Effect': 'Allow',
-                        'Principal': {
-                            'AWS': [
-                                'arn:aws:iam::None:role/adfs-developers',
-                                'arn:aws:iam::None:role/ec2-mlr-test',
-                                'arn:aws:iam::None:role/Ec2-Role'
-                            ]
-                        },
-                        'Action': ['kms:Encrypt', 'kms:Decrypt', 'kms:ReEncrypt*', 'kms:GenerateDataKey*',
-                                   'kms:DescribeKey'],
-                        'Resource': '*'
-                    },
-                    {
-                        'Sid': 'Allow attachment of persistent resources',
-                        'Effect': 'Allow', 'Principal': {'AWS': 'arn:aws:iam::None:role/Ec2-Role'},
-                        'Action': ['kms:CreateGrant', 'kms:ListGrants', 'kms:RevokeGrant'],
-                        'Resource': '*', 'Condition': {'Bool': {'kms:GrantIsForAWSResource': 'true'}}
-                    }
-                ]
-            },
             Description='IOW WQP-EXTERNAL TEST key',
             KeyUsage='ENCRYPT_DECRYPT',
             Origin='AWS_KMS',
