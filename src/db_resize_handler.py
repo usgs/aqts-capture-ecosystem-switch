0a9bf3dd0b739614da9cdf69a07ae88905be3a2e
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-12-09 14:46:15 -0800

    try moving enableProvisionedConcurrency outside of state machine

--- src/db_resize_handler.py
+++ src/db_resize_handler.py
@@ -99,6 +99,7 @@ def grow_db(event, context):
 
 
 def execute_shrink_machine(event, context):
+    enable_provisioned_concurrency()
     arn = os.environ['SHRINK_STATE_MACHINE_ARN']
     payload = {}
     alarm_state = event["detail"]["state"]["value"]
@@ -109,6 +110,7 @@ def execute_shrink_machine(event, context):
 
 
 def execute_grow_machine(event, context):
+    enable_provisioned_concurrency()
     arn = os.environ['GROW_STATE_MACHINE_ARN']
     payload = {}
     alarm_state = event["detail"]["state"]["value"]
@@ -119,10 +121,10 @@ def execute_grow_machine(event, context):
 
 
 def execute_recover_machine(event, context):
+    enable_provisioned_concurrency()
     arn = os.environ['RECOVER_STATE_MACHINE_ARN']
     payload = {}
     _execute_state_machine(arn, json.dumps(payload))
-
 
 
 def _get_cpu_utilization(db_instance_identifier, period_in_seconds, total_time):
